b693d126b84eedc075049eea78219fb0ddc802a0
Nick Don <nick.don.1102@gmail.com>
2022-10-26 21:19:25 +1100

    making changes to ensure text client API is working

--- text_client/mix.exs
+++ text_client/mix.exs
@@ -14,7 +14,8 @@ defmodule TextClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      included_application: [ :hangman ],
+      extra_applications:   [ :logger  ]
     ]
   end
 
